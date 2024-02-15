edf75b905b7f355ecc4582c6402ff8c3c44c7d51
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-15 16:16:52 +0530

    Created servers

--- server/main.go
+++ server/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to start the GRPC server: %v", err)
+	}
+}
