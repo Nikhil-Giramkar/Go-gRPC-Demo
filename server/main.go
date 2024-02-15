1c111d2cad0893eccaab914b47a809a52d081884
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-15 16:22:40 +0530

    imported pb from proto folder location Created helloServer Registered Greet Service Server

--- server/main.go
+++ server/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	pb "github.com/Nikhil-Giramkar/Go-gRPC-Demo/proto"
+	"google.golang.org/grpc"
 	"log"
 	"net"
-
-	"google.golang.org/grpc"
 )
 
 const (
 	port = ":8080"
 )
+
+type helloServer struct {
+	pb.GreetServiceServer;
+}
 
 func main() {
 	listener, err := net.Listen("tcp", port)
@@ -19,6 +23,10 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+
+	log.Printf("Server started at: %v", listener.Addr())
 
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("Failed to start the GRPC server: %v", err)
